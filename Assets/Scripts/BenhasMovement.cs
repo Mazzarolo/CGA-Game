a6665887c6ae6e16d76295089158bddcd5c22d6a
João <jdrmazzarolo@outlook.com>
2023-10-30 23:54:33 -0300

    Adding benhas

---
+++ Assets/Scripts/BenhasMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BenhasMovement : MonoBehaviour
+{
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            transform.LookAt(target);
+        }
+    }
+}
